1343aad88a25763aa554105c39c0b8ea57e35529
Nicolas Pfeiffer <112817219+HandeeDandee@users.noreply.github.com>
2025-12-17 00:37:31 -0800

    added two more lines of console.log code

--- day-02.js
+++ day-02.js
@@ -1,3 +1,5 @@
 // Day 2 - Starting file
 
 console.log("Day 2 is underway!");
+console.log("This is my second line of code written for this file!");
+console.log("I'm looking forward to learning more about JavaScript");
