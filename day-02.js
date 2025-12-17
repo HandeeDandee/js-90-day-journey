5c0ab1bbbbee6878bb7ddd306822861a957ef531
Nicolas Pfeiffer <112817219+HandeeDandee@users.noreply.github.com>
2025-12-17 00:45:42 -0800

    Day 2 - added two if statements with true and false return statements

--- day-02.js
+++ day-02.js
@@ -3,3 +3,11 @@
 console.log("Day 2 is underway!");
 console.log("This is my second line of code written for this file!");
 console.log("I'm looking forward to learning more about JavaScript");
+
+if ("Hello") {
+    return console.log("This is truthy");
+}
+
+if (0) {
+    return console.log("This is falsy");
+}
