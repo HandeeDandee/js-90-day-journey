d700f9289d4630cebb7a066347e279714a9ecf62
Nicolas Pfeiffer <112817219+HandeeDandee@users.noreply.github.com>
2025-12-17 00:14:49 -0800

    Day 1 - Project setup and JS refresh

---
+++ README.md
@@ -0,0 +1,4 @@
+# JS 90 Day Journey
+
+Goal: Transition into software development using JavaScript
+Time Commitment: 1 hour per day.
---
+++ day-01.js
@@ -0,0 +1,7 @@
+// Day 1 - Setup and Javascript refresh
+
+const goal = "Become a Javascript developer in 90 days";
+const skills = ["HTML", "CSS", "JavaScript"];
+const updatedSkills = skills.map(skills => skills.toUpperCase());
+
+console.log(updatedSkills);
