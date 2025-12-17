479388191ff31db410b855b3dcc7d5fbdb2f15ac
Nicolas Pfeiffer <112817219+HandeeDandee@users.noreply.github.com>
2025-12-17 00:42:33 -0800

    Day 2 - added first two console.log statements

--- day-02.js
+++ day-02.js
@@ -3,4 +3,3 @@
 console.log("Day 2 is underway!");
 console.log("This is my second line of code written for this file!");
 console.log("I'm looking forward to learning more about JavaScript");
-console.log("test");
