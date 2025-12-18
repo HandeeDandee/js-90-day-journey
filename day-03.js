c0b6694daa09a3f45c461ce64a87f2940b739a40
Nicolas Pfeiffer <112817219+HandeeDandee@users.noreply.github.com>
2025-12-17 20:56:48 -0800

    Added a second function that included a parameter (name) that would return the phrase 'Hello' + (name) + '!'.

--- day-03.js
+++ day-03.js
@@ -5,3 +5,19 @@ function sayHello() {
 }
 
 sayHello();
+
+// 1. Create a new function that:
+//   - Takes one parameter
+//   - Logs a message using that parameter
+// 2. Call the function twice:
+//   - Once with a value
+//   - Once without a value
+// 3. Run the file locally and observe the difference
+// 4. Commit and push
+
+function greeting(name) {
+    console.log("Hello " + name + "!");
+}
+
+greeting("Nick");
+greeting();
