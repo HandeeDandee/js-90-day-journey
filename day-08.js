991db913c52f7dc7198fe82ddbeba08954122e1a
Nicolas Pfeiffer <112817219+HandeeDandee@users.noreply.github.com>
2025-12-22 15:37:53 -0800

    Day 8 - updated function to have a for loop that logs each value from inputArray.

--- day-08.js
+++ day-08.js
@@ -5,7 +5,11 @@
 let values = [1, 2, 42, "dog", true, "17", 12]
 
 function getOnlyNumbers(inputArray) {
-    result = [];
+    let result = [];
+    for (let i = 0; i < inputArray.length; i++) {
+        console.log(inputArray[i]);
+
+    }
     return result;
 }
 
