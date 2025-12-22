481b08e6c6d2441855ce2a72552e291ff3f08885
Nicolas Pfeiffer <112817219+HandeeDandee@users.noreply.github.com>
2025-12-22 15:42:02 -0800

    Day 8 - Added an if statement to the for loop and typeof to check for and only return numbers from the array.

--- day-08.js
+++ day-08.js
@@ -7,8 +7,9 @@ let values = [1, 2, 42, "dog", true, "17", 12]
 function getOnlyNumbers(inputArray) {
     let result = [];
     for (let i = 0; i < inputArray.length; i++) {
-        console.log(inputArray[i]);
-
+        if (typeof inputArray[i] === "number") {
+            console.log(inputArray[i]);
+        }
     }
     return result;
 }
